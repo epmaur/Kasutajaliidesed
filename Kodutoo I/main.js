550372727034f1e81029904af3917a49e1e69492
Epu <eppmauring@gmail.com>
2018-02-25 23:22:00 +0200

    additional info done, small tweaks and bugfixes

--- Kodutoo I/main.js
+++ Kodutoo I/main.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
     displayNumbers();
     
     $('input[name="rights"]').on('change', function() {
+				$('#block-six').show();
         const val = $(this).val();
-        $('#block-four').show();
         displayNumbers();
         if (val == 1) {
             $('#extras-header').html('Ruumi omamise tõend');
@@ -49,7 +49,20 @@ $(document).ready(function() {
     });
     
     $('#send1').click(function() {
-        if (pageFilled()) {
+			$('.error-message').hide();
+            $('#page1').fadeOut(500);
+            setTimeout(function() {
+                $('.animationload').show();
+            }, 400);
+            setTimeout(function() {
+                $('.animationload').hide();
+				makeSecondPage();
+                $('#page2').fadeIn(500);
+            }, 1000);
+						
+			/*
+        if (pageFilled() && radioButtonChecked()) {
+						$('.error-message').hide();
             $('#page1').fadeOut(500);
             setTimeout(function() {
                 $('.animationload').show();
@@ -59,8 +72,10 @@ $(document).ready(function() {
                 $('#page2').fadeIn(500);
             }, 1000);
         } else {
-            // display modal
+						showBlankFields();
+						$('.error-message').show();
         }
+				*/
     });
     $('#send2').click(function() {
         $('#page2').fadeOut(500);
@@ -84,10 +99,41 @@ $(document).ready(function() {
     });
 });
 
+function makeSecondPage() {
+  	appendAnotherPerson($('#submitter-firstname').val() + ' ' + $('#submitter-lastname').val());
+  	Array.prototype.forEach.call(document.getElementsByClassName('other-people'), function(input) {
+	  	let firstname = $(input).find('#firstname').val();
+	  	let lastname = $(input).find('#lastname').val();
+	  	if (firstname.length > 0 && lastname.length > 0) {
+		  appendAnotherPerson(firstname + ' ' + lastname);
+		}
+    });
+}
+
+function appendAnotherPerson(name) {
+  $('#block-seven').append(
+  '<div class="form-group" id="other-people"><span class="name-span">'+ name +'</span><div class="inputdiv"><input type="text" class="form-control" required><span class="floating-label">Rahvus</span></div><div class="inputdiv"><input type="text" class="form-control" required><span class="floating-label">Emakeel</span></div><select class="form-control select"><option disabled selected>Omandatud kõrgeim haridustase</option><option>Alghariduseta</option><option>Põhikooli 1.-6.klass</option><option>Põhikooli 7.-9.klass</option><option>Kutsekeskharidus või kutseõpe (sh keskeri-või tehnikumiharidus) põhihariduse baasil</option><option>Keskharidus</option><option>Kutsekeskharidus või kutseõpe keskhariduse baasil</option><option>Keskeri- ja tehnikumiharidus keskhariduse baasil</option><option>Bakalaureus või sellega võrdsustatud haridus</option><option>Magister või sellega võrdsustatud haridus</option><option>Doktor või sellega võrdsustatud haridus</option></select><select class="form-control select"><option disabled selected>Sotsiaal-majanduslik seisund</option><option>Töötav</option><option>Kodune</option><option>Töötu/ tööd otsiv</option><option>Ajateenija</option><option>Üli(õpilane)</option><option>Pensionär</option><option>Lapsehoolduspuhkusel</option><option>Muu mittetöötav</option></select></div>'
+  );
+}
+
+function showBlankFields() {
+	let optionalFields = ["comment", "foreignID", "foreignResidence"];
+	Array.prototype.forEach.call(document.getElementsByClassName('required'), function(input) {
+        if ($(input).val() === '' && $.inArray($(input).attr('id'), optionalFields) == -1) {
+            $(input).parent().addClass("has-warning");
+        }
+    });
+}
+
+function radioButtonChecked() {
+	return $('input[name=rights]:checked').length;
+}
+
 function pageFilled() {
     let ok = true;
+		let optionalFields = ["comment", "foreignID", "foreignResidence"];
     Array.prototype.forEach.call(document.getElementsByClassName('required'), function(input) {
-        if ($(input).val() === '') {
+        if ($(input).val() === '' && $.inArray($(input).attr('id'), optionalFields) == -1) {
             ok = false;
         }
     });
@@ -130,7 +176,7 @@ function showInfo(blocks, index, el) {
 
 function addAnotherPerson() {
     $('#block-three #people-wrapper').append(
-        '<div class="form-group" id="other-people"><div class="inputdiv"><input class="form-control"><span class="floating-label">Eesnimi</span></div><div class="inputdiv"><input class="form-control"><span class="floating-label">Perenimi</span></div><div class="inputdiv"><input class="form-control"><span class="floating-label">Isikukood</span></div><div class="inputdiv"><input type="email" class="form-control"><span class="floating-label">E-post</span></div><div class="inputdiv"><input type="tel" class="form-control"><span class="floating-label">Telefon</span></div></div>');
+        '<div class="form-group other-people" id="other-people"><div class="inputdiv"><input class="form-control" required id="firstname"><span class="floating-label">Eesnimi</span></div><div class="inputdiv"><input class="form-control" required id="lastname"><span class="floating-label">Perenimi</span></div><div class="inputdiv"><input class="form-control" required><span class="floating-label">Isikukood</span></div><div class="inputdiv"><input type="email" class="form-control" required><span class="floating-label">E-post</span></div><div class="inputdiv"><input type="tel" class="form-control" required><span class="floating-label">Telefon</span></div></div>');
       
 }
 
