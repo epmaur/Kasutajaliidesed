55c69b55895c6e3e11c0b096ccdf7c710d751f11
Kert Ojasaar <kertojasaar@gmail.com>
2018-05-02 14:38:07 +0300

    Updates.

--- Kodutoo III/js/main.js
+++ Kodutoo III/js/main.js
@@ -1,7 +1,7 @@
 const STUDENTS_TABLE_NAME = 'ui_t3_142766_students';
 const WORK_TABLE_NAME = 'ui_t3_142766_work';
-var students = null;
-getFromDb(STUDENTS_TABLE_NAME);
+var students = [{"id":"1","student_code":"112233IAPB","name":"Kaire Kaalikas","student_group":"K16"},{"id":"2","student_code":"070707IAPB","name":"Peeter Paun","student_group":"K12"},{"id":"3","student_code":"080808IAPB","name":"Robin Kuur","student_group":"K12"},{"id":"4","student_code":"142766IAPB","name":"Kert Ojasaar","student_group":"K16"},{"id":"5","student_code":"123123IAPB","name":"test","student_group":"K14"},{"id":"6","student_code":"111111IAPB","name":"test","student_group":"K12"},{"id":"7","student_code":"111111IAPB","name":"test123","student_group":"K12"},{"id":"8","student_code":"000000IAPB","name":"Testija Test","student_group":"K12"},{"id":"9","student_code":"000000IAPB","name":"Testija Test","student_group":"K12"}];
+// getFromDb(STUDENTS_TABLE_NAME);
 console.log('students:', students);
 
 $(document).ready(function() {
