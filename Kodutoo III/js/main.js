adbcade9ef8629ab05cb116598667311809b1b8e
Kert Ojasaar <kertojasaar@gmail.com>
2018-05-02 14:36:38 +0300

    Updates.

--- Kodutoo III/js/main.js
+++ Kodutoo III/js/main.js
@@ -1,37 +1,47 @@
 const STUDENTS_TABLE_NAME = 'ui_t3_142766_students';
 const WORK_TABLE_NAME = 'ui_t3_142766_work';
+var students = null;
+getFromDb(STUDENTS_TABLE_NAME);
+console.log('students:', students);
 
 $(document).ready(function() {
   // console.log('ready');
-  postToDb(STUDENTS_TABLE_NAME);
-  setTimeout(function() {
-    getFromDb(STUDENTS_TABLE_NAME);
-    // getFromDb(WORK_TABLE_NAME);
-  }, 1000);
+  // postToDb(STUDENTS_TABLE_NAME);
+
+  // getFromDb(WORK_TABLE_NAME);
+
+  $('#submit').click(function() {
+    console.log('student1_code:', $('#student1_code').val());
+    const student1 = students.filter(x => x.student_code === $('#student1_code').val())[0];
+    console.log('student1:', student1);
+    const data = {
+      'tableName': STUDENTS_TABLE_NAME,
+      'due_date': $('').val()
+    };
+  });
 });
 
 function getFromDb(tableName) {
   $.ajax({
-    url: 'api.php',
+    url: 'get.php',
     type: 'GET',
     data: {
-      'tableName': tableName,
-      'method': 'GET'
+      'tableName': tableName
     },
     dataType: 'json',
     success: function(data) {
-      console.log('success data:', data)
+      console.log('success data:', data);
+      students = data;
     }
   });
 }
 
 function postToDb(tableName) {
   $.ajax({
-    url: 'api.php',
+    url: 'post.php',
     type: 'POST',
     data: {
       'tableName': tableName,
-      'method': 'POST',
       'student_code': '000000IAPB',
       'name': 'Testija Test',
       'student_group': 'K12'
